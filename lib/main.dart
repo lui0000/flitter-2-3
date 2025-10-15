58f013e44b6ab887f6739fd2a83e43149a2e947e
Eliza <elbondarenko04@gmail.com>
2025-10-15 21:26:59 +0300

    Реализовано индивидуальное здание для работы 5. Доработка главного класса

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'features/access_requests/screens/access_requests_screen.dart';
+import 'package:provider/provider.dart';
 import 'features/access_requests/state/access_requests_state.dart';
+import 'features/access_requests/screens/access_requests_screen.dart';
 
 void main() {
-  runApp(const AccessApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AccessRequestsState(),
+      child: const AccessApp(),
+    ),
+  );
 }
 
 class AccessApp extends StatelessWidget {
