f8b679146f8eeab31d55d7e18ea071910f5366d8
Eliza <elbondarenko04@gmail.com>
2025-11-03 14:06:09 +0300

    Индивидуальное задание к практической работе номер 7, страничная навигация

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'features/access_requests/state/access_requests_state.dart';
-import 'features/access_requests/screens/access_requests_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/employees_screen.dart';
-import 'screens/systems_screen.dart';
-import 'screens/settings_screen.dart';
 import 'screens/horizontal_navigation_screen.dart';
 
 void main() {
@@ -25,15 +20,7 @@ class AccessApp extends StatelessWidget {
     return MaterialApp(
       title: 'Access IDM',
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomeScreen(),
-        '/requests': (context) => const AccessRequestsScreen(),
-        '/employees': (context) => const EmployeesScreen(),
-        '/systems': (context) => const SystemsScreen(),
-        '/settings': (context) => const SettingsScreen(),
-        '/horizontal': (context) => const HorizontalNavigationScreen(),
-      },
+      home: const HorizontalNavigationScreen(),
     );
   }
 }
